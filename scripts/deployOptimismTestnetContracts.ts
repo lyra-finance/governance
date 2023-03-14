57e661a2f27e79ed7885fd0d6f28fdc546177b51
LondonCalamari <lyndnc99@gmail.com>
2023-03-14 14:17:13 +1100

    Script to deploy OptimismBridgeExecutor

---
+++ scripts/deployOptimismTestnetContracts.ts
@@ -0,0 +1,44 @@
+import { getFirstSigner } from "../helpers/helpers";
+import { validateBaseEnvs } from "./utils/validation";
+import { DAY_SEC } from "../test/utils";
+import { getContractAddress } from "./utils/store";
+import { deployAndValidate } from "./utils/deployAndValidate";
+
+async function main(): Promise<void> {
+  validateBaseEnvs();
+
+  const deployer = await getFirstSigner();
+  console.log("deploying with:", deployer.address);
+
+  const ethereumGovernanceExecutor = getContractAddress("Executor", "goerli").address;
+
+  if (!ethereumGovernanceExecutor) {
+    throw Error("Missing executor");
+  }
+
+  const opBridgeExecutor = await deployAndValidate("OptimismBridgeExecutor", deployer, "optimismBridgeExecutor", [
+    // ethereumGovernanceExecutor
+    ethereumGovernanceExecutor,
+    // delay
+    20,
+    // gracePeriod
+    5 * DAY_SEC,
+    // minimumDelay
+    1,
+    // maximumDelay
+    14 * DAY_SEC,
+    // guardian
+    deployer.address,
+  ]);
+
+  console.log(`- opBridgeExecutor: ${opBridgeExecutor.address}`);
+
+  console.log("\n****** Finished Deployment ******");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error: Error) => {
+    console.error(error);
+    process.exit(1);
+  });
